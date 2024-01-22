beede9880d09c9d7b5cee85807c903332e4dabe6
Cineg <cineg7@gmail.com>
2024-01-22 20:35:28 +0100

    basic setup

--- chatapp/base/urls.py
+++ chatapp/base/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
+urlpatterns: list = [
     path("", views.home, name="home"),
-    path("room/", views.room, name="room"),
+    path("room/<str:pk>/", views.room, name="room"),
 ]
--- chatapp/base/views.py
+++ chatapp/base/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+rooms: list[dict] = [
+    {"id": 1, "name": "Name number 1"},
+    {"id": 2, "name": "Test name"},
+    {"id": 3, "name": "Another name"},
+]
+
 
 # Create your views here.
 def home(request: HttpRequest):
-    return render(request, "home.html")
+    context: dict = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request: HttpRequest):
-    return render(request, "room.html")
+def room(request: HttpRequest, pk: str):
+    context: dict | None = None
+    for room in rooms:
+        if room["id"] == int(pk):
+            context = {"room": room}
+
+    return render(request, "base/room.html", context)
