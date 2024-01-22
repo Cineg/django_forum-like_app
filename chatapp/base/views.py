297215bfcf513a5658a40df67ffa3c8953559b85
Cineg <cineg7@gmail.com>
2024-01-22 21:19:13 +0100

    added db fields

--- chatapp/base/views.py
+++ chatapp/base/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-
-rooms: list[dict] = [
-    {"id": 1, "name": "Name number 1"},
-    {"id": 2, "name": "Test name"},
-    {"id": 3, "name": "Another name"},
-]
+from .models import Room
 
 
 # Create your views here.
 def home(request: HttpRequest):
+    rooms = Room.objects.all()
     context: dict = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request: HttpRequest, pk: str):
-    context: dict | None = None
-    for room in rooms:
-        if room["id"] == int(pk):
-            context = {"room": room}
+    room: Room = Room.objects.get(id=int(pk))
+    context: dict[str, Room] = {"room": room}
 
     return render(request, "base/room.html", context)
