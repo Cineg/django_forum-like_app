41d7581101899bef3e284865a6e3a4b9e7ea890c
Cineg <cineg7@gmail.com>
2024-01-23 18:20:09 +0100

    login

--- chatapp/base/views.py
+++ chatapp/base/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.http import (
     HttpRequest,
     HttpResponse,
     HttpResponsePermanentRedirect,
     HttpResponseRedirect,
 )
+from django.contrib.auth.models import User, AbstractBaseUser
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 # Create your views here.
+def loginPage(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        username: str | None = request.POST.get("username")
+        password: str | None = request.POST.get("password")
+
+        try:
+            user: User | AbstractBaseUser | None = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist.")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or password does not exist.")
+
+    context: dict = {}
+    return render(request, "base/login_register.html", context)
+
+
 def home(request: HttpRequest) -> HttpResponse:
     q: str | None = request.GET.get("q")
     if q == None:
